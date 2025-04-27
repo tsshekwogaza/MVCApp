acc2c7ad58c69485aef2b34838a97aa5c22ec75c
Timothy Samuel <timothysamuelshekwogaza@gmail.com>
2025-04-28 00:28:41 +0100

    Hero page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('heropage')
 
-  <section class="text-white text-center py-40 bg-gradient-to-r from-gray-900 via-gray-400 to-indigo-300">
+  <section class="text-gray-900 text-center py-40 bg-gradient-to-r from-gray-800 via-indigo-300 to-gray-800">
     <h1 class="text-5xl md:text-6xl font-bold leading-tight mb-2 drop-shadow-lg">Knowledge is Power</h1>
     <p class="text-xl md:text-2xl font-light mb-8">Gain power through our curated content.</p>
     <a href="homepage"  class="hover:bg-gray-100 transition-colors duration-300 bg-white text-black font-semibold py-2 px-4 rounded shadow-lg">Explore →</a>
